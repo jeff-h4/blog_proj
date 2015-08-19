332267a000178d06d84e7897a25bc46919cd46b8
Jeff <jahuangi@yahoo.ca>
2015-08-18 23:38:24 -0700

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,11 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
+  root 'home#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+get 'home'  => 'home#index'
+get 'about' => 'home#about'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
