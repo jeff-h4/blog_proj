9129964c1cf373f11606b5675fb141e629514ef7
Jeff <jahuangi@yahoo.ca>
2015-08-20 18:12:30 -0700

    added models and migrations, performed migration. Still need to add validation

---
+++ db/migrate/20150821010114_add_name_and_email_and_subject_and_message_to_contact.rb
@@ -0,0 +1,7 @@
+class AddNameAndEmailAndSubjectAndMessageToContact < ActiveRecord::Migration
+  def change
+    add_column :contacts, :name, :string
+    add_column :contacts, :email, :string
+    add_column :contacts, :message, :text
+  end
+end
