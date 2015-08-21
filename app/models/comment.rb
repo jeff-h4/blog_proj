f3d40431476e231a141c52b7e4d39fb24258689b
Jeff <jahuangi@yahoo.ca>
2015-08-20 18:16:15 -0700

    updated models with validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,5 @@
 class Comment < ActiveRecord::Base
+  # Uniqueness supposed to be limited to the blog post.
+  # this might need a change later
+  validates :body, presence: true, uniqueness: true
 end
