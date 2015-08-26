8c2a81de4c28c049995315bf451302e818769dc4
Jeff H <jahuangi@yahoo.ca>
2015-08-26 16:42:12 -0700

    Added users, and user authentication feature

--- config/routes.rb
+++ config/routes.rb
@@ -20,6 +20,11 @@ Rails.application.routes.draw do
     resources :comments, only: [:create, :destroy]
   end
 
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create] do
+    delete :destroy, on: :collection
+  end
+
 
 
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150826005902) do
+ActiveRecord::Schema.define(version: 20150826221502) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,17 @@ ActiveRecord::Schema.define(version: 20150826005902) do
     t.string   "title"
     t.text     "body"
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "password_digest"
+    t.string   "email"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", using: :btree
 
   add_foreign_key "comments", "posts"
 end
